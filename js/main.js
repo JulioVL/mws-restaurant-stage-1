b071fd574a3f0c00d53995017b210e869f71edc8
Julio Venero Loayza <venerojulio@gmail.com>
2018-09-21 19:50:34 -0500

    feat: add tabindex, aria labels to home page

--- js/main.js
+++ js/main.js
@@ -167,18 +167,22 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.tabIndex = 0;
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.tabIndex = 0;
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.tabIndex = 0;
   li.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute("aria-label", restaurant.name + " view details");
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
