07026834ff377daecdb71f96ffa3e235e2be7a1b
Julio Venero Loayza <venerojulio@gmail.com>
2018-09-24 20:52:40 -0500

    feat: service worker registration

---
+++ js/serviceWorker.js
@@ -0,0 +1,7 @@
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register('./sw.js').then(function(reg) {
+		console.log('Working!');
+	}).catch(function(err) {
+		console.log('Not working!');
+	});
+}
