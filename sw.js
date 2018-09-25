b6f56d4a7fbe4a6e388c79780f3780620b8414d6
Julio Venero Loayza <venerojulio@gmail.com>
2018-09-25 18:07:40 -0500

    feat: configure fetch event

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let cacheName = 'v2';
+const cacheName = 'v2';
 
 self.addEventListener('install', function(event) {
 	const urlsToCache = [
@@ -42,3 +42,11 @@ self.addEventListener('activate', function(event) {
 		})
 	)
 })
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
